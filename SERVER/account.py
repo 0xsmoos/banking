daa27c87ee545e640fd89ef5fabbc86f1f29677d
John Dough <liketoaccess@gmail.com>
2021-06-17 15:24:40 +0000

    Update

--- SERVER/account.py
+++ SERVER/account.py
@@ -1,3 +1,5 @@
+import media
+
 class Account:
 	def __init__(self, name, balance=0):
 		self.name = name
@@ -10,7 +12,9 @@ class Account:
 	def subtract(self, amount):
 		self.balance -= amount if amount > 0 else 0
 		return self.balance
-
-	# TODO
-	def transfer(self, amount, recipient):
+		
+	def transfer(self, amount, recipient, sender):
+		sender.subtract(amount)
+		recipient.add(amount)
+		media.write_file(filename, msg)
 		return self.balance
